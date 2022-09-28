f78fc24a8e5281891033153b33569908b10fc63b
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-09-28 23:46:41 +0300

    refactor stete and selector

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { ContactName, ContactNumber, DeleteButton } from './Contact.styled';
 import { useDispatch } from 'react-redux';
-import { deleteContact } from 'redux/contactsSlice';
+// import { deleteContact } from 'redux/contactsSlice';
 
 export const Contact = ({ name, number, id }) => {
   const dispatch = useDispatch();
@@ -10,9 +10,8 @@ export const Contact = ({ name, number, id }) => {
     <>
       <ContactName>{name} :</ContactName>
       <ContactNumber>{number}</ContactNumber>
-      <DeleteButton onClick={() => dispatch(deleteContact(id))}>
-        Delete
-      </DeleteButton>
+      {/* <DeleteButton onClick={() => dispatch(deleteContact(id))}> */}
+      <DeleteButton>Delete</DeleteButton>
     </>
   );
 };
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,7 +2,7 @@ import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 import { Button, Label, Input, Message } from './ContactForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact } from 'redux/contactsSlice';
+// import { addContact } from 'redux/contactsSlice';
 import { Notify } from 'notiflix';
 import { getContacts } from '../../redux/selectors';
 const schema = yup.object().shape({
@@ -49,7 +49,7 @@ export const ContactForm = () => {
       return;
     }
     try {
-      await dispatch(addContact(values));
+      // await dispatch(addContact(values));
     } catch (error) {
       console.log(error);
     }
