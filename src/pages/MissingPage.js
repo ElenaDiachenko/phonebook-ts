9ab4e0690d6a1a344116db7aea57f19515950b39
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-10-07 01:00:43 +0300

    fixed MissingPage

--- src/pages/MissingPage.js
+++ src/pages/MissingPage.js
@@ -1,16 +1,14 @@
-import { Link } from 'react-router-dom';
 import { Box } from 'components/Box';
 import { Message } from 'components/Message/Message';
 import { Container } from 'components/Container/Container';
+import { NavigateButton } from 'components/NavigateButton/NavigateButton';
 
 const MissingPage = () => {
   return (
     <Container>
       <Box display="flex" flexDirection="column" gridGap={3}>
         <Message>Page Not Found</Message>
-        <Message>
-          <Link to="/">Visit Our Homepage</Link>
-        </Message>
+        <NavigateButton path="/">Go Home </NavigateButton>
       </Box>
     </Container>
   );
