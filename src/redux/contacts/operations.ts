576238d62d3f0b9b25741e3b128e93c66e478eba
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-12-11 00:03:11 +0200

    refactored

--- src/redux/contacts/operations.ts
+++ src/redux/contacts/operations.ts
@@ -4,21 +4,22 @@ import { IContact } from 'interfaces/IContact';
 
 const ContactsAPI = contactsAxios;
 
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchAll',
-  async (_, { rejectWithValue }) => {
-    try {
-      const res = await ContactsAPI.get('/contacts');
-      return res.data;
-    } catch (err) {
-      if (err instanceof Error) {
-        return rejectWithValue(err.message);
-      } else {
-        console.log('Unexpected error', err);
-      }
+export const fetchContacts = createAsyncThunk<
+  IContact[],
+  undefined,
+  { rejectValue: string }
+>('contacts/fetchAll', async (_, { rejectWithValue }) => {
+  try {
+    const res = await ContactsAPI.get('/contacts');
+    return res.data;
+  } catch (err) {
+    if (err instanceof Error) {
+      return rejectWithValue(err.message);
+    } else {
+      console.log('Unexpected error', err);
     }
   }
-);
+});
 
 export const addContact = createAsyncThunk<
   IContact,
@@ -36,34 +37,36 @@ export const addContact = createAsyncThunk<
     }
   }
 });
-export const editContact = createAsyncThunk(
-  'contacts/editContact',
-  async ({ id, ...values }: IContact, { rejectWithValue }) => {
-    try {
-      const res = await ContactsAPI.patch(`/contacts/${id}`, values);
-      return res.data;
-    } catch (err) {
-      if (err instanceof Error) {
-        return rejectWithValue(err.message);
-      } else {
-        console.log('Unexpected error', err);
-      }
+export const editContact = createAsyncThunk<
+  IContact,
+  IContact,
+  { rejectValue: string }
+>('contacts/editContact', async ({ id, ...values }, { rejectWithValue }) => {
+  try {
+    const res = await ContactsAPI.patch(`/contacts/${id}`, values);
+    return res.data;
+  } catch (err) {
+    if (err instanceof Error) {
+      return rejectWithValue(err.message);
+    } else {
+      console.log('Unexpected error', err);
     }
   }
-);
+});
 
-export const deleteContact = createAsyncThunk(
-  'contacts/deleteContact',
-  async (contactId, { rejectWithValue }) => {
-    try {
-      const res = await ContactsAPI.delete(`/contacts/${contactId}`);
-      return res.data;
-    } catch (err) {
-      if (err instanceof Error) {
-        return rejectWithValue(err.message);
-      } else {
-        console.log('Unexpected error', err);
-      }
+export const deleteContact = createAsyncThunk<
+  IContact,
+  IContact['id'],
+  { rejectValue: string }
+>('contacts/deleteContact', async (contactId, { rejectWithValue }) => {
+  try {
+    const res = await ContactsAPI.delete(`/contacts/${contactId}`);
+    return res.data;
+  } catch (err) {
+    if (err instanceof Error) {
+      return rejectWithValue(err.message);
+    } else {
+      console.log('Unexpected error', err);
     }
   }
-);
+});
