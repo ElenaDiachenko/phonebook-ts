10b46c908bb6ec0534b25c7a869bdaf2a983ed26
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-12-11 03:22:49 +0200

    refactored MissingPage

--- src/pages/MissingPage.tsx
+++ src/pages/MissingPage.tsx
@@ -2,13 +2,16 @@ import { Box } from 'components/Box';
 import { Message } from 'components/Message/Message';
 import { Container } from 'components/Container/Container';
 import { NavigateButton } from 'components/NavigateButton/NavigateButton';
+import { FaLongArrowAltLeft } from 'react-icons/fa';
 
 const MissingPage = () => {
   return (
     <Container>
       <Box display="flex" flexDirection="column" gridGap={3}>
-        <Message>Page Not Found</Message>
-        <NavigateButton path="/">Go Home </NavigateButton>
+        <Message>Page Not Found. Go Home</Message>
+        <NavigateButton path="/">
+          <FaLongArrowAltLeft />
+        </NavigateButton>
       </Box>
     </Container>
   );
